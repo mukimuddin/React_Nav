6178b379ffac1742fd5407daf5af160b3771392f
Md Mukim Uddin <mdmukimuddin.bd@gmail.com>
2024-09-09 23:25:38 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ function App() {
   
   return (
     <>
-      
+      <nav className='w-full py-10 bg-black'>
+        <ul className='flex gap-6'>
+          <li><a href="#" className='text-[red] text-[22px]'>Contact</a></li>
+          <li><a href="#" className='text-[red] text-[22px]'>Blog</a></li>
+          <li><a href="#" className='text-[red] text-[22px]'>About</a></li>
+          <li className='relative group'><a href="#" className='text-[red] text-[22px]'>Plans</a>
+          <ul className='w-[250px] p-5 bg-white rounded-lg flex flex-col items-center gap-3 top-[60px] absolute invisible group-hover:visible'>
+            <li><a className='' href="#">Hello</a></li>
+            <li><a className='' href="#">Hello</a></li>
+          </ul>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
