5440763a899f78d1ed71e10e7152d956721127ac
Md Mukim Uddin <mdmukimuddin.bd@gmail.com>
2024-09-20 08:41:45 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,22 @@ function App() {
   return (
     <>
       <nav className='w-full py-10 bg-black'>
-        <ul className='flex gap-6'>
-          <li><a href="#" className='text-[red] text-[22px]'>Contact</a></li>
-          <li><a href="#" className='text-[red] text-[22px]'>Blog</a></li>
+        <ul className='flex justify-center gap-6'>
+          <li className='relative group flex justify-center'><a href="#" className='text-[red] text-[22px]'>Contact</a>
+          <ul className='w-[250px] p-5 bg-white rounded-lg flex flex-col items-center gap-3 top-[40px] absolute invisible group-hover:visible transition-all'>
+            <li><a className='' href="#">Mobile No.</a></li>
+            <li><a className='' href="#">Gmail</a></li>
+          </ul>
+          </li>
+          <li className='relative group flex justify-center'><a href="#" className='text-[red] text-[22px]'>Blog</a>
+          <ul className='w-[250px] p-5 bg-white rounded-lg flex flex-col items-center gap-3 top-[40px] absolute invisible group-hover:visible transition-all'>
+            <li><a className='' href="#">Hello</a></li>
+            <li><a className='' href="#">Hello</a></li>
+          </ul>
+          </li>
           <li><a href="#" className='text-[red] text-[22px]'>About</a></li>
-          <li className='relative group'><a href="#" className='text-[red] text-[22px]'>Plans</a>
-          <ul className='w-[250px] p-5 bg-white rounded-lg flex flex-col items-center gap-3 top-[60px] absolute invisible group-hover:visible'>
+          <li className='relative group flex justify-center'><a href="#" className='text-[red] text-[22px]'>Plans</a>
+          <ul className='w-[250px] p-5 bg-white rounded-lg flex flex-col items-center gap-3 top-[40px] absolute invisible group-hover:visible transition-all'>
             <li><a className='' href="#">Hello</a></li>
             <li><a className='' href="#">Hello</a></li>
           </ul>
